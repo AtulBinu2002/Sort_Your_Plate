8e96a424bebc9fdf2abe209c761650d7b38b7e21
AtulBinu2002 <atulbinu01@gmail.com>
2023-10-22 22:23:36 +0530

    Helper Function

---
+++ Helper/Data.py
@@ -0,0 +1,126 @@
+import json
+import random 
+import os
+import shutil
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tqdm import tqdm
+
+def put_labels(Data_path,label_path):
+   """
+    Accepts a label path (in the form of a JSON) and creates a file
+    as a Python object.
+   """
+   onlyfiles = [f for f in os.listdir(Data_path) if os.isfile(os.join(Data_path, f))]
+   with open("sample.json", "w") as outfile:
+    json.dump(onlyfiles, outfile)
+
+def get_labels(label_path):
+    """
+    Accepts a label path (in the form of a JSON) and returns the file
+    as a Python object.
+    """
+    with open(label_path) as f:
+        return json.load(f)
+
+# Create a function to import an image and resize it to be able to be used with our model
+def load_and_prep_image(filename, img_shape=224, scale=True):
+  """
+  Reads in an image from filename, turns it into a tensor and reshapes into
+  (224, 224, 3).
+
+  Parameters
+  ----------
+  filename (str): string filename of target image
+  img_shape (int): size to resize target image to, default 224
+  scale (bool): whether to scale pixel values to range(0, 1), default True
+  """
+  # Read in the image
+  img = tf.io.read_file(filename)
+  # Decode it into a tensor
+  img = tf.image.decode_jpeg(img)
+  # Resize the image
+  img = tf.image.resize(img, [img_shape, img_shape])
+  if scale:
+    # Rescale the image (get all values between 0 and 1)
+    return img/255.
+  else:
+    return img
+
+def get_percent_images(target_dir, new_dir, sample_amount=0.1, random_state=42):
+    """
+    Get sample_amount percentage of random images from target_dir and copy them to new_dir.
+    
+    Preserves subdirectory file names.
+    
+    E.g. target_dir=pizza_steak/train/steak/all_files 
+                -> new_dir_name/train/steak/X_percent_of_all_files
+                
+    Parameters
+    --------
+    target_dir (str) - file path of directory you want to extract images from
+    new_dir (str) - new directory path you want to copy original images to
+    sample_amount (float), default 0.1 - percentage of images to copy (e.g. 0.1 = 10%)
+    random_state (int), default 42 - random seed value 
+    """
+    # Set random seed for reproducibility
+    random.seed(random_state)
+    
+    # Get a list of dictionaries of image files in target_dir
+    # e.g. [{"class_name":["2348348.jpg", "2829119.jpg"]}]
+    images = [{dir_name: os.listdir(target_dir + dir_name)} for dir_name in os.listdir(target_dir)]
+
+    for i in images:
+        for k, v in i.items():
+            # How many images to sample?
+            sample_number = round(int(len(v)*sample_amount))
+            print(f"There are {len(v)} total images in '{target_dir+k}' so we're going to copy {sample_number} to the new directory.")
+            print(f"Getting {sample_number} random images for {k}...")
+            random_images = random.sample(v, sample_number)
+
+            # Make new dir for each key
+            new_target_dir = new_dir + k
+            print(f"Making dir: {new_target_dir}")
+            os.makedirs(new_target_dir, exist_ok=True)
+
+            # Keep track of images moved
+            images_moved = []
+
+            # Create file paths for original images and new file target
+            print(f"Copying images from: {target_dir}\n\t\t to: {new_target_dir}/\n")
+            for file_name in tqdm(random_images):
+                og_path = target_dir + k + "/" + file_name
+                new_path = new_target_dir + "/" + file_name
+
+                # Copy images from OG path to new path
+                shutil.copy2(og_path, new_path)
+                images_moved.append(new_path)
+
+            # Make sure number of images moved is correct
+            assert len(os.listdir(new_target_dir)) == sample_number
+            assert len(images_moved) == sample_number
+
+
+# Make a function to predict on images and plot them (works with multi-class)
+def pred_and_plot(model, filename, class_names):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  if len(pred[0]) > 1: # check for multi-class
+    pred_class = class_names[pred.argmax()] # if more than one output, take the max
+  else:
+    pred_class = class_names[int(tf.round(pred)[0][0])] # if only one output, round
+
+  # Plot the image and predicted class
+  plt.imshow(img)
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False);
+
