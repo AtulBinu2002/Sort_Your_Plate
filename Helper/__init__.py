9e9428055586b6ded37d2f94193e50b56f438d77
AtulBinu2002 <atulbinu01@gmail.com>
2023-10-23 18:47:10 +0530

    Helper

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,6 +1,7 @@
 #__int__.py
 import os
 os.makedirs("Data\Raw_image\\",exist_ok=True)
+os.makedirs("Data\Mixed_Data\\",exist_ok=True)
 os.makedirs("Graph\\",exist_ok=True)
 os.makedirs("JSON\\",exist_ok=True)
 os.makedirs("Model\\",exist_ok=True)
